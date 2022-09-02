9fdaa7ae476294e8eb380b70edb720d925b08729
Max Kovalyov <max.kov86@ukr.net>
2022-09-03 00:06:02 +0300

    Долав розмітку та js

---
+++ www/js/main.js
@@ -0,0 +1,67 @@
+var stroka = "World";
+
+switch (stroka) {
+  case "4":
+    console.log("Переменная со значением 4");
+    break;
+  case "45":
+    console.log("Переменная со значением 45");
+    break;
+  case "Word":
+    console.log("Переменная со значением: World");
+    break;
+  default:
+    console.log("Default");
+}
+
+// var number = 15;
+// var isHasHouse = true;
+
+// if (number == 15 && !isHasHouse) {
+//   console.log("Ok.");
+// } else if (number == 20) {
+//   console.log("Yes!");
+// } else if (number >= 30) {
+//   console.log("Yes!");
+// } else if (number > 10) {
+//   console.log("Yes!");
+// } else {
+//   console.log("Else!");
+// }
+//   document.write("JavaScript говорит привет!");
+//   console.log("JavaScript говорит привет!");
+//   console.info("JavaScript говорит привет!");
+// console.error("JavaScript говорит привет!");
+// console.warn("JavaScript говорит привет!");
+// var num;
+// num = 5;
+// const num = 5;
+// var num = 5;
+// num = 7;
+// console.log("Переменная: " + num + ".");
+
+// var number;
+// number = true;
+
+// number = "stroka";
+
+// var num_1 = 5;
+// var num_2 = "5";
+// var num_1 = 5;
+// var num_2 = 15;
+
+// console.log("Результат:" + num_1 * num_2);
+
+// var num_3 = 5;
+// num_3--;
+
+// console.log("Результат:" + num_3);
+
+// var str_1 = Number("12");
+// var str_2 = Number("2");
+
+// console.log("Результат:" + (str_1 + str_2));
+
+// console.log("Math:" + Math.PI);
+// console.log("Math:" + Math.min(3, 1, -9, 7, 4, 6));
+// console.log("Math:" + Math.max(3, 1, -9, 7, 4, 6));
